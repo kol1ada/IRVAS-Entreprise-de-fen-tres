33e4a5042f9306445f95ba936b58261edad1f6a1
Koliada Oleksii <koliada.oleksii@gmail.com>
2023-02-21 18:34:29 +0100

    +timer

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,20 @@ import './slider';
 import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
+import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
+
+    let modalState = {};
+    let deadline = '2023-04-01';
+
     modals()
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active')
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click')
-    forms()
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block')
+    changeModalState(modalState)
+    forms(modalState)
+    timer('.container1', deadline)
 })
